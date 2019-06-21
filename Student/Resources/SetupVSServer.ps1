91452d3f6cd5395babc92bcbc2253056052f790e
kvitaly2005 <kvitaly2005@live.com>
2019-06-21 15:50:31 +0300

    Update SetupVSServer.ps1

--- Student/Resources/SetupVSServer.ps1
+++ Student/Resources/SetupVSServer.ps1
@@ -25,7 +25,7 @@ $zipFileeShopTemp = [System.IO.Path]::GetTempPath().ToString() + "eShopOnWeb-mas
 if (Test-Path $zipFileeShopTemp) { Remove-Item $zipFileeShopTemp -Force }
 $zipFileeShop = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName "eShopOnWeb-master.zip" -PassThru
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://github.com/dotnet-architecture/eShopOnWeb/archive/master.zip" -OutFile $zipFileeShop
+Invoke-WebRequest -Uri "https://github.com/via-clouds/eShopOnWeb/archive/master.zip" -OutFile $zipFileeShop
 $BackUpPath = $zipFileeShop.FullName
 New-Item -Path c:\eshoponweb -ItemType directory -Force
 $Destination = "C:\eshoponweb"
